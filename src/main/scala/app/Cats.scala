8e158b695622d931bb3a04ab307f940bdb06f84d
Ayanga Okpokowuruk <ayanga@aok-solutions.com>
2020-02-02 23:33:19 -0600

    A.3 | printable interface syntax

--- src/main/scala/app/Cats.scala
+++ src/main/scala/app/Cats.scala
@@ -3,8 +3,10 @@ package app
 import models.Cat
 import printable.Printable
 import printable.PrintableInstances._
+import printable.PrintableSyntax._
 
 object Cats extends App {
   val cat = Cat("Garfield", 12, "Orange")
   Printable.print(cat)
+  cat.print
 }
