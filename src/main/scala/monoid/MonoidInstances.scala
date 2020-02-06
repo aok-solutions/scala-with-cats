aab855048d82ef5a10b824be2e7a67a0782362c9
Ayanga Okpokowuruk <ayanga@aok-solutions.com>
2020-02-05 22:10:47 -0600

    B.2 | set monoids/semigroups

--- src/main/scala/monoid/MonoidInstances.scala
+++ src/main/scala/monoid/MonoidInstances.scala
@@ -1,6 +1,7 @@
 package monoid
 
 object MonoidInstances {
+  //  ---------------- Boolean Monoids ----------------
   implicit val andMonoid: Monoid[Boolean] = new Monoid[Boolean] {
     def empty: Boolean = true
     def combine(x: Boolean, y: Boolean): Boolean = x && y
@@ -19,5 +20,20 @@ object MonoidInstances {
   implicit val exclusiveNorMonoid: Monoid[Boolean] = new Monoid[Boolean] {
     def empty: Boolean = true
     def combine(x: Boolean, y: Boolean): Boolean = (!x || y) && (x || !y)
+  }
+
+  //  ---------------- Set Monoids/Semigroups ----------------
+  implicit def unionMonoid[A]: Monoid[Set[A]] = new Monoid[Set[A]] {
+    def empty: Set[A] = Set.empty
+    def combine(x: Set[A], y: Set[A]): Set[A] = x union y
+  }
+
+  implicit def intersectionSemigroup[A]: Semigroup[Set[A]] = new Semigroup[Set[A]] {
+    def combine(x: Set[A], y: Set[A]): Set[A] = x intersect y
+  }
+
+  implicit def symmetricDifferenceMonoid[A]: Monoid[Set[A]] = new Monoid[Set[A]] {
+    def empty: Set[A] = Set.empty
+    def combine(x: Set[A], y: Set[A]): Set[A] = x.diff(y) union y.diff(x)
   }
 }
