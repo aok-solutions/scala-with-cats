6101f665e7d32201633266b2e9e4a9bb7c48f361
Ayanga Okpokowuruk <ayanga@aok-solutions.com>
2020-02-09 17:47:01 -0600

    D.1 | map for monad

---
+++ src/main/scala/monad/Monad.scala
@@ -0,0 +1,7 @@
+package monad
+
+trait Monad[F[_]] {
+  def pure[A](value: A): F[A]
+  def flatMap[A, B](value: F[A])(f: A ⇒ F[B]): F[B]
+  def map[A,B](value: F[A])(f: A ⇒ B): F[B] = flatMap(value)(a ⇒ pure(f(a)))
+}
