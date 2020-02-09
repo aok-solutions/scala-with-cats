349a07aed8c452f80f94e3d8977b4d10359be447
Ayanga Okpokowuruk <ayanga@aok-solutions.com>
2020-02-09 16:21:16 -0600

    C.6 | codec for box

--- src/main/scala/codec/CodecInstances.scala
+++ src/main/scala/codec/CodecInstances.scala
@@ -1,5 +1,7 @@
 package codec
 
+import models.Box
+
 object CodecInstances {
   implicit val stringCodec: Codec[String] = new Codec[String] {
     def encode(value: String): String = value
@@ -9,4 +11,5 @@ object CodecInstances {
   implicit val intCodec: Codec[Int] = stringCodec.imap(_.toInt, _.toString)
   implicit val booleanCodec: Codec[Boolean] = stringCodec.imap(_.toBoolean, _.toString)
   implicit val doubleCodec: Codec[Double] = stringCodec.imap(_.toDouble, _.toString)
+  implicit def boxCodec[A](implicit c: Codec[A]): Codec[Box[A]] = c.imap[Box[A]](Box(_), _.value)
 }
